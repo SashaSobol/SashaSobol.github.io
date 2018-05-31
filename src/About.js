e04f6a6bc6041f92538dca1c01f7d8d3457bf9ae
SashaSobol <Alexandrbzgroup@gmail.com>
2018-05-31 15:12:07 +0300

    Revert "React"
    This reverts commit 87b8bac401a92925151f6e400437f46706852ab1.

--- src/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class About extends Component {
-    render() {
-        return <div>This is our cool music app</div>;
-    }
-}
-
-export default About;
